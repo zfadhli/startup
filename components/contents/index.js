b33dcd2ac72bdb69b9f962c1401ea4b9c4930312
Zulfadhli Zakari <zulfadhli4141@gmail.com>
2020-09-29 19:05:11 +0800

    feat: add content

---
+++ components/contents/index.js
@@ -0,0 +1,67 @@
+import content1 from './content-1'
+import content2 from './content-2'
+import content3 from './content-3'
+import content4 from './content-4'
+import content5 from './content-5'
+import content6 from './content-6'
+import content7 from './content-7'
+import content8 from './content-8'
+import content9 from './content-9'
+import content10 from './content-10'
+import content11 from './content-11'
+import content12 from './content-12'
+import content13 from './content-13'
+import content14 from './content-14'
+import content15 from './content-15'
+import content16 from './content-16'
+import content17 from './content-17'
+import content18 from './content-18'
+import content19 from './content-19'
+import content20 from './content-20'
+import content21 from './content-21'
+import content22 from './content-22'
+import content23 from './content-23'
+import content24 from './content-24'
+import content25 from './content-25'
+import content26 from './content-26'
+import content27 from './content-27'
+import content28 from './content-28'
+import content29 from './content-29'
+import content30 from './content-30'
+import content31 from './content-31'
+import content32 from './content-32'
+
+export default {
+  one: content1,
+  two: content2,
+  three: content3,
+  four: content4,
+  five: content5,
+  six: content6,
+  seven: content7,
+  eight: content8,
+  nine: content9,
+  ten: content10,
+  eleven: content11,
+  twelve: content12,
+  thirteen: content13,
+  fourteen: content14,
+  fifteen: content15,
+  sixteen: content16,
+  seventeen: content17,
+  eighteen: content18,
+  nineteen: content19,
+  twenty: content20,
+  twentyone: content21,
+  twentytwo: content22,
+  twentythree: content23,
+  twentyfour: content24,
+  twentyfive: content25,
+  twentysix: content26,
+  twentyseven: content27,
+  twentyeight: content28,
+  twentynine: content29,
+  thirty: content30,
+  thirtyone: content31,
+  thirtytwo: content32,
+}
