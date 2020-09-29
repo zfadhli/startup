b04d5f4890091815eb46bd994714924c8303bb95
Zulfadhli Zakari <zulfadhli4141@gmail.com>
2020-09-29 19:06:00 +0800

    feat: add title tag

--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,34 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container mx-auto text-lg">
-      <h2>Tailwindcss</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit ut saepe, quidem quia
-        reiciendis eveniet recusandae fuga pariatur quaerat sit, culpa vitae aliquam praesentium
-        doloremque aspernatur eum velit illo tempore.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ea perspiciatis, minima
-        corporis ad rem perferendis cupiditate eos possimus voluptatibus qui recusandae explicabo
-        facere provident necessitatibus nisi labore. Repellendus itaque architecto laborum
-        perferendis porro magni dicta tenetur! Eaque cum libero quod, odit voluptatibus perferendis
-        asperiores eum eius, iusto recusandae neque illo, eveniet nam illum dicta doloribus
-        molestias enim incidunt quaerat fugit maxime eos iure? Minus, est eligendi. Sunt laborum
-        vero quisquam quas dicta eaque nam, dolores explicabo enim reiciendis voluptates voluptate
-        quis, possimus, repudiandae rem. Natus atque quae, suscipit aliquid necessitatibus beatae
-        mollitia deleniti quam sunt totam repellat laboriosam iure?
-      </p>
-      <div>
-        <button className="px-4 py-2 bg-blue-500 text-white rounded shadow text-sm font-bold">
-          Send
-        </button>
+    <>
+      <Head>
+        <title>Home - Startup</title>
+      </Head>
+      <div className="container mx-auto text-lg">
+        <h2>Tailwindcss</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit ut saepe, quidem quia
+          reiciendis eveniet recusandae fuga pariatur quaerat sit, culpa vitae aliquam praesentium
+          doloremque aspernatur eum velit illo tempore.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ea perspiciatis, minima
+          corporis ad rem perferendis cupiditate eos possimus voluptatibus qui recusandae explicabo
+          facere provident necessitatibus nisi labore. Repellendus itaque architecto laborum
+          perferendis porro magni dicta tenetur! Eaque cum libero quod, odit voluptatibus
+          perferendis asperiores eum eius, iusto recusandae neque illo, eveniet nam illum dicta
+          doloribus molestias enim incidunt quaerat fugit maxime eos iure? Minus, est eligendi. Sunt
+          laborum vero quisquam quas dicta eaque nam, dolores explicabo enim reiciendis voluptates
+          voluptate quis, possimus, repudiandae rem. Natus atque quae, suscipit aliquid
+          necessitatibus beatae mollitia deleniti quam sunt totam repellat laboriosam iure?
+        </p>
+        <div>
+          <button className="px-4 py-2 bg-blue-500 text-white rounded shadow text-sm font-medium">
+            Send
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
