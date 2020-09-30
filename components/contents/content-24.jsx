90c3b6972646fd9eb6ffb90f9c405c17dbce5b27
Zulfadhli Zakari <zulfadhli4141@gmail.com>
2020-09-30 11:31:02 +0800

    feat: add content 24 & 25

--- components/contents/content-24.jsx
+++ components/contents/content-24.jsx
@@ -1,60 +1,44 @@
+const Icon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+    />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
+)
+
 export default function content() {
   return (
     <>
-      <div className="py-20 bg-white">
-        <div className="max-w-2xl mx-auto space-y-8 text-center">
-          <div className="flex items-center justify-center space-x-16">
-            <div className="w-12 h-12">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
-                />
-              </svg>
-            </div>
-            <div className="w-12 h-12 text-red-500">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
-            </div>
-            <div className="w-12 h-12">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                />
-              </svg>
+      <div className="py-20 bg-white h-128">
+        <div
+          class="relative shadow bg-indigo-400 rounded-lg max-w-4xl mx-auto w-full h-full"
+          style={{
+            backgroundImage:
+              "url('https://images.unsplash.com/photo-1578836537282-3171d77f8632?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=75')",
+            backgroundRepeat: 'no-repat',
+            backgroundSize: 'cover',
+            backgroundBlendMode: 'multiply',
+          }}
+        >
+          <div className="absolute p-8">
+            <span className="px-4 py-2 text-sm font-bold text-red-600 uppercase bg-white rounded shadow">
+              zfadhli
+            </span>
+          </div>
+          <div className="flex items-center justify-center w-full h-full">
+            <div className="w-32 h-32 text-white">
+              <Icon />
             </div>
           </div>
-          <h2 className="text-4xl font-bold">Better Landing for Your Startup</h2>
-          <p className="text-gray-500">
-            We have created a new product that will help designers, developers and companies create
-            websites for their startups quickly and easily.
-          </p>
         </div>
       </div>
     </>
