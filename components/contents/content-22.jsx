bf03bd7121af0e1e91976802d977a8d2075e3274
Zulfadhli Zakari <zulfadhli4141@gmail.com>
2020-09-30 09:50:55 +0800

    feat: add content 20 & 21

--- components/contents/content-22.jsx
+++ components/contents/content-22.jsx
@@ -1,60 +1,20 @@
 export default function content() {
   return (
     <>
-      <div className="py-20 bg-white">
-        <div className="max-w-2xl mx-auto space-y-8 text-center">
-          <div className="flex items-center justify-center space-x-16">
-            <div className="w-12 h-12">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
-                />
-              </svg>
-            </div>
-            <div className="w-12 h-12 text-red-500">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
-            </div>
-            <div className="w-12 h-12">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                />
-              </svg>
-            </div>
+      <div className="relative h-128">
+        <div className="absolute z-0 w-full h-full overflow-hidden bg-indigo-700 shadow">
+          <img
+            src={require('@/assets/bg-02.jpg')}
+            alt="bg image"
+            className="object-cover object-center"
+          />
+        </div>
+        <div className="h-full max-w-4xl mx-auto">
+          <div className="relative z-10 flex flex-row items-end justify-end h-full pb-8">
+            <span className="px-4 py-2 text-sm font-bold text-white uppercase bg-indigo-700 rounded shadow">
+              zulfadhli zakari
+            </span>
           </div>
-          <h2 className="text-4xl font-bold">Better Landing for Your Startup</h2>
-          <p className="text-gray-500">
-            We have created a new product that will help designers, developers and companies create
-            websites for their startups quickly and easily.
-          </p>
         </div>
       </div>
     </>
