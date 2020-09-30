9b1a26b09fdd5684e97b5ed833027bf561c57909
Zulfadhli Zakari <zulfadhli4141@gmail.com>
2020-09-30 10:17:07 +0800

    fix: grid height

--- components/contents/content-23.jsx
+++ components/contents/content-23.jsx
@@ -2,11 +2,11 @@ export default function content() {
   return (
     <>
       <div className="py-20 bg-white">
-        <div className="max-w-4xl mx-auto space-y-8 text-center">
+        <div className="max-w-6xl mx-auto space-y-8 text-center">
           <div
             className="grid grid-cols-3 gap-8"
             style={{
-              gridTemplateRows: 'repeat(4, 192px)',
+              gridTemplateRows: 'repeat(4, 200px)',
             }}
           >
             <div className="col-span-1 row-start-1 row-end-4 overflow-hidden rounded-lg shadow">
